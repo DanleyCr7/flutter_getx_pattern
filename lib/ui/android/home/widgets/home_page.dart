d7b5bb5de1efba9cf6ec5d7dde8ff75c42c32c17
unknown <Danrleysil46@gmail.com>
2022-03-12 21:43:58 -0300

    lista de clientes

--- lib/ui/android/home/widgets/home_page.dart
+++ lib/ui/android/home/widgets/home_page.dart
@@ -25,20 +25,62 @@ class HomePage extends GetView<HomeController> {
                 )
               : ListView.builder(
                   itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(_.postList[index].title),
-                      subtitle: Text(_.postList[index].body),
-                      onTap: () async {
-                        await Get.put(DetailsController(
-                                repository: MyRepository(
-                                    apiClient: MyApiClient(
-                                        httpClient: http.Client()))))
-                            .editar(_.postList[index].id);
-                        Get.to(DetailsPage(), binding: DetailsBinding());
-                      },
+                    return GestureDetector(
+                      child: Container(
+                          child: ListTile(
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 20.0, vertical: 10.0),
+                              leading: Container(
+                                padding: EdgeInsets.only(right: 12.0),
+                                decoration: new BoxDecoration(
+                                    border: new Border(
+                                        right: new BorderSide(
+                                            width: 1.0, color: Colors.black))),
+                                child: CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                      'https://1.bp.blogspot.com/-fco8CRivKY8/XglVbwzvVRI/AAAAAAAAeao/G1CQ_ljrt1AsLAfolHsQYGV_TuGAi-jNACNcBGAsYHQ/s1600/Kimetsu-no-Yaiba-Demon%2BSlayer-wallpapers-para-celular-tanjiro%2B%25283%2529.jpg'),
+                                ),
+                              ),
+                              title: Text(
+                                _.postList[index].title,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              // subtitle: Text("Intermediate", style: TextStyle(color: Colors.black)),
+
+                              subtitle: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  Flexible(
+                                      child: Text(_.postList[index].body,
+                                          overflow: TextOverflow.ellipsis,
+                                          maxLines: 1,
+                                          softWrap: false,
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                          ))),
+                                  IconButton(
+                                      onPressed: () {
+                                        print('Editar');
+                                      },
+                                      icon: Icon(Icons.edit,
+                                          color: Colors.black, size: 25.0)),
+                                  IconButton(
+                                      onPressed: () {
+                                        print('Editar');
+                                      },
+                                      icon: Icon(Icons.delete_forever_sharp,
+                                          color: Colors.red, size: 25.0))
+                                ],
+                              ),
+                              trailing: null)),
+                      onTap: () => {},
                     );
                   },
-                  itemCount: _.postList.length,
                 );
         }),
       ),
